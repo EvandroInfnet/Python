b8a28b947fb31b805b96ad445f14dfa4b1fb33f4
Evandro Alves <35381788-evandroalves4@users.noreply.replit.com>
2024-07-24 23:51:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#quebra de linha
+print("Hello World\n\tMeu Nome é Evandro")
+#quebra de linha diferente
+print("""Hello World
+Meu Nome é Evandro""")
+
+
+#imprimir string
+texto_para_print = "Hello"
+
+print(texto_para_print, "Mundo")
